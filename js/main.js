8ddfa0dd2c87753d9539d087b8c6a57ee17faee3
Marcin Sornek <marcinsornek@gmail.com>
2017-11-27 01:08:29 +0100

    Add js code and index.html to show results

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // main script
+function TriangleArea(a, h) {
+    return a*h/2;
+}
+
+function ShowResult() {
+    triangleArea = document.getElementById('result');
+    a = document.getElementById('a').value;
+    h = document.getElementById('h').value;
+		if (isNaN(a) || isNaN(h)) {
+			alert('Base or height isn\'t a number');
+			triangleArea.innerText = 'Try with number ;)';
+		} else {
+    	triangleArea.innerText = TriangleArea(a,h);
+		}
+}
